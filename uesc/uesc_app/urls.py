b0d6e8b49b41b74567da49bd4f848c84567e2db5
gsousa143 <gabrielsousa4242@gmail.com>
2024-04-13 12:53:43 -0300

    atualizando banco de dados e adicionando paginas de editais e noticias

--- uesc/uesc_app/urls.py
+++ uesc/uesc_app/urls.py
@@ -3,4 +3,6 @@ from uesc_app import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path("noticias/",views.noticias,name="noticias"),
+    path("editais/",views.editais,name="editais")
 ]
--- uesc/uesc_app/views.py
+++ uesc/uesc_app/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from uesc_app.models import Tipo, Grupo
+from uesc_app.models import *
 
 def home(request):
     tipos = Tipo.objects.all()
     grupos_por_tipo = {}
     
     for tipo in tipos:
-        grupos_por_tipo[tipo] = Grupo.objects.filter(tipo=tipo).prefetch_related('link_set').all()
+        grupos_por_tipo[tipo] = Grupo.objects.filter(
+            tipo=tipo).prefetch_related('link_set').all()
     
     contexto = {'grupos_por_tipo': grupos_por_tipo}
     return render(request, "index.html", contexto)
+
+def noticias(request):
+    noticias = Tipo.objects.all().filter(
+        tipo="noticias").prefetch_related('link_set').all()
+    contexto = {'noticias': noticias}
+    return render(request, "noticias.html", contexto)
+
+def editais(request):
+    editais = Tipo.objects.all().filter(
+        tipo="editais").prefetch_related('link_set').all()
+    contexto = {'editais': editais}
+    return render(request, "editais.html", contexto)
+
+
