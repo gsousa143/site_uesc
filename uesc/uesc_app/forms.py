7b685a82ebe62a3ed494774912920eb6b4c62ae2
gsousa143 <gabrielsousa4242@gmail.com>
2024-04-26 07:25:28 -0300

    estilizando forms

--- uesc/uesc_app/forms.py
+++ uesc/uesc_app/forms.py
@@ -6,14 +6,34 @@ class LinkForm (forms.ModelForm):
     class Meta:
         model = Link
         fields = ['nome','endereco','grupo']
-    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nome'].widget = forms.TextInput()
+        self.fields['nome'].widget.attrs.update(
+            {'placeholder':"Titulo do Link", 'class': 'form-control'})
+        self.fields['endereco'].widget.attrs.update(
+            {'placeholder':"Endereço do Link", 'class': 'form-control'})
+        self.fields['grupo'].widget.attrs.update(
+            {'placeholder':"Selecione um Grupo", 'class': 'form-control'})
+
 class GrupoForm (forms.ModelForm):
     class Meta:
         model = Grupo
         fields = ['tipo','titulo']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['titulo'].widget.attrs.update(
+            {'placeholder':"Titulo do Grupo", 'class': 'form-control'})
+        self.fields['tipo'].widget.attrs.update(
+            {'placeholder':"Selecione um Tipo", 'class': 'form-control'})
 
 
 class TipoForm (forms.ModelForm):
     class Meta:
         model = Tipo
         fields = ['tipo']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tipo'].widget.attrs.update(
+            {'placeholder':"Titulo do Tipo", 'class': 'form-control'})
